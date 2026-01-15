6944aa2b84b5041c4a707be0324a7beefb65deb6
Gonzalo Blanco Mosteiro <gblamos249@g.educaand.es>
2026-01-15 14:05:23 +0100

    Ejercicio 1 y 2

--- src/main/kotlin/org/iesra/Main.kt
+++ src/main/kotlin/org/iesra/Main.kt
@@ -1,16 +1,53 @@
 package org.iesra
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+// Ejercicio 1
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+class CuentaBancaria(private var titular: String, private var saldo: Double = 0.0)
+{
+    fun ingresar(cantidad: Double)
+    {
+        if (cantidad <= 0) throw Exception("La cantidad no puede ser negativa o 0")
+
+        saldo += cantidad
+        println("Saldo: $saldo")
     }
+
+    fun retirar(cantidad: Double)
+    {
+        if (cantidad >= saldo) throw Exception("No se pueden retirar cantidades mayores al saldo")
+
+        saldo -= cantidad
+        println("Saldo: $saldo")
+    }
+}
+
+
+// Ejercicio 2
+class Vehiculo(private var marca: String, private var modelo: String, private var kilometraje: Double = 0.0)
+{
+    fun registrarViaje(kilometros: Double)
+    {
+        // No se saben los detalles
+    }
+
+    fun detalles(): String { return "Vehiculo $marca, modelo $modelo con $kilometraje km" }
+}
+
+
+// Ejercicio 3
+class Libro(private var titulo: String, private var autor: String)
+{
+    constructor(numPaginas: Int, leido: Boolean): this(titulo, autor) {
+        numPaginas = 100
+        leido = false
+    }
+
+    override fun toString(): String
+    { return "Libro: $titulo por $autor, paginas: $numPaginas, leido: ${if (leido) "Si" else "No"}" }
+}
+
+
+fun main()
+{
+
 }
